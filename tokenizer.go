8154a6a226eade9e93db5866d31d23daead954bc
Kamil <kamil.figon@gmail.com>
2022-10-02 20:22:23 +0200

    lexer done

--- tokenizer.go
+++ tokenizer.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
 
 type tokenType int
 
@@ -16,7 +20,8 @@ const (
 )
 
 func (t tokenType) String() string {
-	return []string{"number",
+	return []string{
+		"number",
 		"plus",
 		"minus",
 		"multiply",
@@ -36,5 +41,38 @@ func (t token) String() string {
 }
 
 func parseExpression(ex expressionCell) []token {
-	return nil
+	out := []token{}
+	singleCharTokens := map[byte]tokenType {
+		'+': plus,
+		'-': minus,
+		'*': multiply,
+		'(': openParent,
+		')': closeParent,
+		'=': equal,
+	}
+
+	i := 0
+	for i < len(ex.exp) {
+		c := ex.exp[i]
+		if t, ok := singleCharTokens[c]; ok {
+			out = append(out, token{t, string(c)})
+			i++
+		} else if unicode.IsSpace(rune(c)){
+			i++
+		} else {
+			str := ""
+			for i < len(ex.exp) && (unicode.IsDigit(rune(ex.exp[i])) || unicode.IsLetter(rune(ex.exp[i]))) {
+				str += string(ex.exp[i])
+				i++
+			}
+			
+			if _, err := strconv.Atoi(str); err == nil {
+				out = append(out, token{number, str})
+			} else if _, ok := parseCoords(str); ok {
+				out = append(out, token{coord, str})
+			}
+		}
+	}
+
+	return out
 }
