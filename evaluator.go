c8427f24e181c1e102f76fce3e8e7fe96396b20e
Kamil <kamil.figon@gmail.com>
2022-10-05 18:46:44 +0200

    token iterator

--- evaluator.go
+++ evaluator.go
@@ -19,6 +19,42 @@ func (e *evaluator) eval(currentCoord string, exp expressionCell) (int, error) {
 	if len(tokens) < 2 {
 		return -1, fmt.Errorf("invalid expression provided")
 	}
+
+	iter := &tokenIterator{tokens: tokens}
+	iter.next() // =
+
+	for iter.hasNext() {
+		iter.next()		
+	}
 	
 	return -1, nil
+}
+
+type tokenIterator struct {
+	tokens []token
+	i int
+}
+
+func (t *tokenIterator) hasNext() bool {
+	return t.i < len(t.tokens)
+}
+
+func (t *tokenIterator) next() {
+	if t.hasNext() {
+		t.i++
+	}
+}
+
+func (t *tokenIterator) currentToken() (token, bool) {
+	if t.hasNext() {
+		return t.tokens[t.i], true
+	}
+	return token{}, false
+}
+
+func (t *tokenIterator) peek() (token, bool) {
+	if t.i+1 < len(t.tokens) {
+		return t.tokens[t.i+1], true
+	}
+	return token{}, false
 }
