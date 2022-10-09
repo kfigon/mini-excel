c0120ff61cf26ad21ce8103d8c70906ca21d425d
Kamil <kamil.figon@gmail.com>
2022-10-09 20:49:55 +0200

    simple evaluator in main

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/sqs/goreturns/returns"
 )
 
 func main() {
@@ -13,4 +15,28 @@ func main() {
 	}
 	path := os.Args[1]
 	fmt.Println(path)
+
+	d, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		return
+	}
+
+	ss := spreadsheetParser{"\n", ","}.parse(string(d))
+	for header,rows := range ss {
+		for k,v := range rows {
+			exp, ok := v.(expressionCell)
+			if !ok {
+				continue
+			}
+			v, err := newEvaluator(ss).eval(fmt.Sprintf("%v%v", header,k), exp)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			ss[header][k] = numberCell(v)
+		}
+	}
+
+	fmt.Println(ss)
 }
